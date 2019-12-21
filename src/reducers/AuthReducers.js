5f619c2b58c23356fb2bb4504d4a49e46b73e704
jeanluc <mtmjea001@myuct.ac.za>
2019-12-21 03:58:06 +0200

    reducers

--- src/reducers/AuthReducers.js
+++ src/reducers/AuthReducers.js
@@ -1,19 +1,42 @@
 import {
-    USER_AUTH_FORM_UPDATE,
-    RESET_FORM
+    RESET_FORM, USER_AUTH_FORM_UPDATE,
+    USER_SIGNIN, USER_SIGNIN_SUCCESS, USER_SIGNIN_FAIL,
+    USER_SIGNOUT, USER_SIGNOUT_FAIL, USER_SIGNOUT_SUCCESS,
+    USER_SIGNUP, USER_SIGNUP_FAIL, USER_SIGNUP_SUCCESS,
 } from '../actions/types';
 const INITIAL_STATE={
     fullName:'',
     email:'',
     passWord:'',
-    confirmPassword: ''
+    confirmPassword: '',
+    loading: false,
+    error:''
 };
 export default (state=INITIAL_STATE, action)=>{
     switch(action.type) {
-        case USER_AUTH_FORM_UPDATE:
-            return {...state,[action.payload.prop]: action.payload.value};
         case RESET_FORM:
             return INITIAL_STATE;
+        case USER_AUTH_FORM_UPDATE:
+            return {...state,[action.payload.prop]: action.payload.value};
+        case USER_SIGNUP:
+            return {...state, loading:true}
+        case USER_SIGNUP_FAIL:
+            return {...state, loading:false, error:'Error. Try diffrent email'}
+        case USER_SIGNUP_SUCCESS:
+                return {...state, loading:false, error:'',...action.payload}
+        case USER_SIGNIN:
+                return {...state, loading:true}
+        case USER_SIGNIN_FAIL:
+            return {...state, loading:false, error:'Error. Check email/password'}
+        case USER_SIGNIN_SUCCESS:
+                return {...state, loading:false, error:'',...action.payload}
+        case USER_SIGNOUT:
+                return {...state, loading:true}
+        case USER_SIGNOUT_FAIL:
+            return {...state, loading:false, error:'Error. Sign out again'}
+        case USER_SIGNOUT_SUCCESS:
+                return INITIAL_STATE;
+
         default:
             return state;
     }
--- src/reducers/PropertyReducers.js
+++ src/reducers/PropertyReducers.js
@@ -1,20 +1,42 @@
 import {
-RESET_FORM,
-PROPERTY_FORM_UPDATE
+    RESET_FORM, PROPERTY_FORM_UPDATE,
+    PROPERTY_CREATE,PROPERTY_CREATE_FAIL, PROPERTY_CREATE_SUCCESS,
+    PROPERTY_EDIT, PROPERTY_EDIT_SUCCESS, PROPERTY_EDIT_FAIL,
+    PROPERTY_DELETE, PROPERTY_DELETE_SUCCESS, PROPERTY_DELETE_FAIL,
 } from '../actions/types';
 
 const INITIAL_STATE = {
     name:'',
     address: '',
-    price: ''
+    price: '',
+    loading: false,
+    error: '',
 }
 
 export default (state=INITIAL_STATE, action) => {
     switch(action.type) {
-        case PROPERTY_FORM_UPDATE:
-            return {...state, [action.payload.prop]:action.payload.value};
         case RESET_FORM:
             return INITIAL_STATE;
+        case PROPERTY_FORM_UPDATE:
+            return {...state, [action.payload.prop]:action.payload.value};
+        case PROPERTY_CREATE:
+            return {...state, loading:true}
+        case PROPERTY_CREATE_FAIL:
+            return {...state, loading:false, error: 'Failed. Try again, Later'}
+        case PROPERTY_CREATE_SUCCESS:
+            return INITIAL_STATE;
+        case PROPERTY_EDIT:
+            return {...state, loading:true}
+        case PROPERTY_EDIT_FAIL:
+            return {...state, loading:false, error: 'Failed. Try again, Later'}
+        case PROPERTY_EDIT_SUCCESS:
+            return INITIAL_STATE;
+        case PROPERTY_DELETE:
+            return {...state, loading:true}
+        case PROPERTY_DELETE_FAIL:
+            return {...state, loading:false, error: 'Failed. Try again, Later'}
+        case PROPERTY_DELETE_SUCCESS:
+            return INITIAL_STATE;        
         default:
             return state;
     }
